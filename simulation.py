8174dc5ebb2d243d4b659f481d3bf92b9ebb25b0
Maxime Alain <myoxsis@gmail.com>
2022-08-07 12:57:55 +0200

    Outbound processes added

--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ import simpy, random, statistics
 import numpy as np
 import matplotlib.pyplot as plt
 
-def warehouse_run(env, reception_team, inspection_team, putaway_team):
+def inbound_run(env, reception_team, inspection_r_team, putaway_team):
     global cost 
 
     cost = 0.0
@@ -16,9 +16,26 @@ def warehouse_run(env, reception_team, inspection_team, putaway_team):
         yield env.timeout(generate_shipment_arrival())
         print(f"{env.now} -  Shipment n°{i} inbound")
         env.process(goods_receipt(env, reception_team, i))
-        env.process(inspection_at_reception(env, inspection_team, i))
+        yield env.timeout(generate_interactivity_time())
+        env.process(inspection_at_reception(env, inspection_r_team, i))
+        yield env.timeout(generate_interactivity_time())
         env.process(putaway(env, putaway_team, i))
 
+        i += 1
+
+def outbound_run(env, picking_team, packing_team, inspection_s_team, shipping_team):
+    i = 0
+    while True:
+        # inbound process
+        yield env.timeout(generate_delivery_arrival())
+        print(f"{env.now} -  Delivery n°{i} inbound")
+        env.process(picking(env, picking_team, i))
+        yield env.timeout(generate_interactivity_time())
+        env.process(packing(env, packing_team, i))
+        yield env.timeout(generate_interactivity_time())
+        env.process(inspection_at_shipping(env, inspection_s_team, i))
+        yield env.timeout(generate_interactivity_time())
+        env.process(shipping(env, shipping_team, i))
         i += 1
     
 def goods_receipt(env, reception_team, name):
@@ -27,11 +44,11 @@ def goods_receipt(env, reception_team, name):
         yield env.timeout(generate_reception())
     print(f"{env.now} -  Shipment {name} received")
 
-def inspection_at_reception(env, inspection_team, name):
+def inspection_at_reception(env, inspection_r_team, name):
     inspection_flag = np.random.randint(1, 10)
     if inspection_flag >= 8:
         print(f"{env.now} -  Shipment {name} : inspection required")
-        with inspection_team.request() as request:
+        with inspection_r_team.request() as request:
             yield request
             yield env.timeout(generate_insp_recp())
         print(f"{env.now} -  Shipment {name} inspected")
@@ -44,72 +61,123 @@ def putaway(env, putaway_team, name):
         yield env.timeout(generate_putaway())
     print(f"{env.now} -  Shipment {name} entered in stock")
 
+def picking(env, picking_team, name):
+    with picking_team.request() as request:
+        yield request
+        yield env.timeout(generate_picking())
+    print(f"{env.now} -  Delivery {name} picked")
+
+def packing(env, packing_team, name):
+    with packing_team.request() as request:
+        yield request
+        yield env.timeout(generate_packing())
+    print(f"{env.now} -  Delivery {name} packed")
+
+def inspection_at_shipping(env, inspection_s_team, name):
+    inspection_flag_r = np.random.randint(1, 10)
+    if inspection_flag_r >= 6:
+        print(f"{env.now} -  Delivery {name} : inspection required")
+        with inspection_s_team.request() as request:
+            yield request
+            yield env.timeout(generate_insp_ship())
+        print(f"{env.now} -  Delivery {name} inspected")
+    else:
+        pass
+
+def shipping(env, shipping_team, name):
+    with shipping_team.request() as request:
+        yield request
+        yield env.timeout(generate_shipping())
+    print(f"{env.now} -  Delivery {name} shipped")
+
 def generate_shipment_arrival():
     return np.random.uniform(1/8)
 
+def generate_delivery_arrival():
+    return np.random.uniform(4/8)
+
+def generate_interactivity_time():
+    return np.random.uniform(1/8, 4/8)
+
 def generate_reception():
-    return 1 #np.random.uniform(2, 6)
+    return 1/8 #np.random.uniform(2, 6)
 
 def generate_insp_recp():
-    return 1
+    return 6/8
+
+def generate_insp_ship():
+    return 6/8
 
 def generate_putaway():
-    return 1
+    return 1/8
+
+def generate_picking():
+    return 1/8
+
+def generate_packing():
+    return 3/8
+
+def generate_shipping():
+    return 8/8
 
 
-def convert_nb_to_time():
+def convert_nb_to_time(x):
     """
         Generate a function to convert the simulated time to day and hours
     """
-    pass
+    day = int(x)
+    hours = x % 1
+    #day = x - hours
 
-def convert_nb_to_time():
+    hours = hours*100
+    print(hours)
+    hours = hours/24
+    mins = (hours/24)/60
+    
+    
+    
+
+    #hours = hours*24
+    #mins = hours % 1
+    #hours = hours - mins
+    #mins = mins*100
+    print(hours)
+    print(mins)
+    t_date = f"Day {int(day+1)}, {int(hours)}:{int(mins)}"
+
+    return t_date
+
+def convert_time_to_nb():
     """
         Generate a function to convert the time to simulated time
     """
     pass
 
 
-############
-
-#def operate_machine(env, repairers, spares, name):
-#    global cost
-
-#    while True:
-#        yield env.timeout(generate_time_to_failure())
-#        t_broken = env.now
-#        print(f"Machine {name} broke {t_broken}")
-#        # Launch repair process
-#        env.process(repair_machine(env, repairers, spares, name))
-#        yield spares.get(1)
-#        t_replaced = env.now
-#        print(f"Machine {name} Replaced {t_replaced}")
-
-#        cost = 20 * (t_replaced - t_broken)
-
-#def repair_machine(env, repairers, spares, name):
-#    with repairers.request() as request:
-#        yield request
-#        yield env.timeout(generate_repair_time())
-#        yield spares.put(1)
-#    print(f"Machine {name} repaired {env.now}")
-
-#def generate_time_to_failure():
-#    return np.random.uniform(132, 185)
-
-#def generate_repair_time():
-#    return np.random.uniform(4, 10) 
+x = 1.4434353
+print(x)
+print(convert_nb_to_time(x))
 
 
 env = simpy.Environment()
 reception_team = simpy.Resource(env, capacity=2)
-inspection_team = simpy.Resource(env, capacity=1)
+inspection_r_team = simpy.Resource(env, capacity=1)
 putaway_team = simpy.Resource(env, capacity=1)
+picking_team = simpy.Resource(env, capacity=2)
+packing_team = simpy.Resource(env, capacity=1)
+inspection_s_team = simpy.Resource(env, capacity=1)
+shipping_team = simpy.Resource(env, capacity=1)
 #spares = simpy.Container(env, init=20, capacity=20)
-env.process(warehouse_run(env, reception_team, inspection_team, putaway_team))
+env.process(inbound_run(env, reception_team, inspection_r_team, putaway_team))
+env.process(outbound_run(env, picking_team, packing_team, inspection_s_team, shipping_team))
 
-env.run(until=8)
+env.run(until=8*5)
 
+
+#class Warehouse():
+#    def __init__(self, reception_team_capacity, ):
+#        self.env = simpy.Environment()
+#        self.reception_team = simpy.Resource
 
 
 
