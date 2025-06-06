1d5aec45811c544fc9cd861a7cc63ab72454a532
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2025-06-07 01:18:40 +0200

    fix electron esm startup

--- main.mjs
+++ main.mjs
@@ -1,5 +1,9 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Create the main application window
 function createWindow() {
